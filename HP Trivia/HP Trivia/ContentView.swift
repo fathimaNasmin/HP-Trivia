d5c3fd847c142e6def2d0982e75edcc867665f1a
Nasmin <fathimanesmi@gmail.com>
2025-01-05 18:20:36 -0500

    Setup the project
    Added all the resources for the project

---
+++ HP Trivia/HP Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HP Trivia
+//
+//  Created by Fathima Nasmin on 1/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
