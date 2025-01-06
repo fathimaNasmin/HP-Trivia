12353d2f25e6a0e72d9a45f4a2939ffe3d7b942f
Nasmin <fathimanesmi@gmail.com>
2025-01-06 07:41:24 -0500

    Layout the title screen

---
+++ HP Trivia/HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Fathima Nasmin on 1/6/25.
+//
+
+import Foundation
+
+enum Constants {
+	static let hpFont = "PartyLetPlain"
+}
--- HP Trivia/HP Trivia/ContentView.swift
+++ HP Trivia/HP Trivia/ContentView.swift
@@ -9,13 +9,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		GeometryReader { geo in
+			ZStack {
+				Image("hogwarts")
+					.resizable()
+					.frame(width: geo.size.width * 3, height: geo.size.height)
+					.padding(.top, 3)
+				
+				VStack {
+					VStack {
+						Image(systemName: "bolt.fill")
+							.font(.largeTitle)
+							.imageScale(.large)
+						
+						Text("HP")
+							.font(.custom(Constants.hpFont, size: 70))
+							.padding(.bottom, -50)
+							
+						Text("Trivia")
+							.font(.custom(Constants.hpFont, size: 60))
+					}
+					.padding(.top, 70)
+					
+					Spacer()
+					
+					// score board
+					
+					VStack {
+						Text("Recent Scores")
+							.font(.title2)
+						
+						Text("33")
+						Text("23")
+						Text("12")
+					}
+					.font(.title3)
+					.padding(.horizontal)
+					.foregroundColor(.white)
+					.background(.black.opacity(0.7))
+					.cornerRadius(10)
+					
+					Spacer()
+					
+					HStack {
+						Spacer()
+						
+						Button {
+							// show instruction screen
+						} label: {
+							Image(systemName: "info.circle.fill")
+								.font(.largeTitle)
+								.foregroundColor(.white)
+								.shadow(color: .black, radius: 5)
+						}
+						
+						Spacer()
+						
+						//Play button
+						Button {
+							// Play button action
+						} label: {
+							Text("Play")
+								.font(.title2)
+								.foregroundColor(.white)
+								.padding(.vertical, 7)
+								.padding(.horizontal, 50)
+								.background(.brown)
+								.cornerRadius(7)
+						}
+						
+						Spacer()
+						
+						// settings button
+						Button {
+							// settings action
+						} label: {
+							Image(systemName: "gearshape.fill")
+								.font(.largeTitle)
+								.foregroundColor(.white)
+								.shadow(color: .black, radius: 5)
+						}
+						
+						Spacer()
+					}
+					.frame(width: geo.size.width)
+					
+					Spacer()
+				}
+			}
+			.frame(width: geo.size.width, height: geo.size.height)
+			
+		}
+		.ignoresSafeArea()
     }
 }
 
